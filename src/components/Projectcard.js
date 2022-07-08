4f659b119b40a06ba14ac474e76c75d25118976b
Emilio Zanoni <emiliozanoni@hotmail.com>
2022-07-08 10:47:15 -0300

    Initial commit

---
+++ src/components/Projectcard.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Projectcard(props){
+    return(
+        <div className="card-container">
+            <img className="card-image" src={props.img} alt="project-img"/>
+            <div className="card-text">
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-description">{props.desc}</p>
+            </div>
+            <button className="card-btn">Live demo!</button>
+        </div>
+    )
+}
+
+export default Projectcard
